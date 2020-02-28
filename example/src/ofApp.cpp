569fbdb5557d29a422685917fca8132ccfcfb238
Ben Snell <bensnell@localprojects.com>
2020-02-28 16:39:46 -0500

    Updated example model to reflect this version of fdeep

--- example/src/ofApp.cpp
+++ example/src/ofApp.cpp
@@ -3,7 +3,7 @@
 //--------------------------------------------------------------
 void ofApp::setup(){
 
-	model = std::make_unique<fdeep::model>(fdeep::load_model(ofToDataPath("assets/models/fldc_pi_38_1550_fdeep_model.json")));
+	model = std::make_unique<fdeep::model>(fdeep::load_model(ofToDataPath("assets/models/fldc_pi_38_1550_fdeep_model_v0.12.0-p0.json")));
 
 	/////// SINGLE ////////
 
