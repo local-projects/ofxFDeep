28b0222eb244f34c5953e4817b1984d6f4b7565d
Ben Snell <bensnell@localprojects.com>
2020-02-28 16:44:35 -0500

    Updated fdeep to version 0.12.1 -- this is faster than 0.12.0 and updated the provided example model to reflect this

--- example/src/ofApp.cpp
+++ example/src/ofApp.cpp
@@ -3,7 +3,7 @@
 //--------------------------------------------------------------
 void ofApp::setup(){
 
-	model = std::make_unique<fdeep::model>(fdeep::load_model(ofToDataPath("assets/models/fldc_pi_38_1550_fdeep_model_v0.12.0-p0.json")));
+	model = std::make_unique<fdeep::model>(fdeep::load_model(ofToDataPath("assets/models/fldc_pi_38_1550_fdeep_model_v0.12.1-p0.json")));
 
 	/////// SINGLE ////////
 
